8ced756e4a04edd879f06ef2928dd2f44be3f331
Lukmon <lonabulel@gmail.com>
2023-03-20 21:46:53 +0000

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,10 @@ int main(void)
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
-	{
 		putchar (i);
-		if (i != '9');
+	if (i != '9')
 		putchar(',');
-		putchar(' ');
-	}
+	putchar(' ');
 	putchar('\n');
 
 	return (0);
