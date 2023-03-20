38edec3a4f0d0b92adeea60e4c88d478d6ed0f48
Lukmon <lonabulel@gmail.com>
2023-03-20 19:04:09 +0000

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	char dh;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (dh = 'A'; dh <= 'Z'; ch++)
+		putchar(dh);
+	putchar('\n');
+
+	return (0);
+}
+
+
+
