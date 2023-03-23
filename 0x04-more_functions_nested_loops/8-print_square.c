7c048a6e9450541981fd8279cef51940ffec0341
Lukmon <lonabulel@gmail.com>
2023-03-23 23:53:58 +0000

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,12 +12,12 @@ void print_square(int size)
 	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
-		{			
+		{
 			for (j = 1; j <= size; j++)
 			_putchar(35);
 		_putchar('\n');
 		}
-	}	
+	}
 	else
 		_putchar('\n');
 }
