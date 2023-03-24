f49659650b0c01b3ab979723f975c4507c15667b
Lukmon <lonabulel@gmail.com>
2023-03-24 00:34:09 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,12 +9,14 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-		if ((i % 3) == 0)
-			printf(" Fizz");
-		else if ((i % 5) == 0)
-			printf(" Buzz");
+		if (i == 1)
+			printf("%d", i);
 		else if ((i % 15) == 0)
 			printf(" FizzBuzz");
+		else if ((i % 5) == 0)
+			printf(" Buzz");
+		else if ((i % 3) == 0)
+			printf(" Fizz");
 		else
 			printf(" %d", i);
 	return (0);
