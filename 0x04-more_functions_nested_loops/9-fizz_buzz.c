1eb5836f88410c81028b233ffd466ec3e338b94e
Lukmon <lonabulel@gmail.com>
2023-03-24 00:24:02 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+		if ((i % 3) == 0)
+			printf(" Fizz");
+		else if ((i % 5) == 0)
+			printf(" Buzz");
+		else if ((i % 15) == 0)
+			printf(" FizzBuzz");
+		else
+			printf(" %d", i);
+	return (0);
+}
