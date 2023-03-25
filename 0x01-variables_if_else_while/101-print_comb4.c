c9c7894d56fee956a95341cb3fcf49e6b9fbe3cb
Lukmon <lonabulel@gmail.com>
2023-03-25 10:36:26 +0000

    possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = '0';
+
+	while (i <= '7')
+	{
+		j = i + 1;
+
+		while (j <= '8')
+		{
+			k = j + 1;
+
+			while (k <= '9')
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i != '7')
+				{
+					putchar('.');
+					putchar(' ');
+				}
+
+				k++;
+			}
+
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
