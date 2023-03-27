dcf3af0819cd22fc096effdd8b49fa18cafc753b
Lukmon <lonabulel@gmail.com>
2023-03-27 20:49:17 +0000

    returns langht of inputted string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,9 +2,15 @@
 /**
  * _strlen - prints the lenght of a string
  *
- * @s: string inputted 
+ * @s: string inputted
  * Return: the lenght of 's'
  */
-int_strlen(char *s)
+int _strlen(char *s)
 {
+	int i = 0;
 
+	while (s[i] != '\0')
+		i++;;
+
+	return (i);
+}
