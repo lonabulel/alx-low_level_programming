ce8cccdae8ccef90e15ead4cb965f6c299941eda
Lukmon <lonabulel@gmail.com>
2023-03-27 23:00:19 +0000

    No 4 - No 9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,8 @@ int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
 void puts2(char *str);
+void print_array(int *a, int n);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
