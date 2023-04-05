2e892c7ba70e155d321f07e07180b030443b5f34
Lukmon <lonabulel@gmail.com>
2023-04-05 23:22:21 +0000

    length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,9 +11,7 @@ int _strlen_recursion(char *s)
 
 	if (*s == '\0')
 		return (0);
-	else
-	{
-		length = 1 + _strlen_recursion(s + 1);
-		return (length);
-	}
+	length = 1 + _strlen_recursion(s + 1);
+	return (length);
+
 }
