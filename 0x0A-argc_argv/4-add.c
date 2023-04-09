36356bb67deabeb8da8396aa536b8ab799e6f06d
Lukmon <lonabulel@gmail.com>
2023-04-09 14:25:03 +0000

    sum of positive integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds all the positive numbers
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ * Return: 0;
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (argv[i][0] > 65 && argv[i][0] < 122)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum = sum + atoi(argv[i]);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
