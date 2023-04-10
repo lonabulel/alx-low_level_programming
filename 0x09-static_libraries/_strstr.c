967b5e0f00e3d35da6dab05c8847537279f8ba7a
Lukmon <lonabulel@gmail.com>
2023-04-10 19:30:21 +0000

    static library

---
+++ 0x09-static_libraries/_strstr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ * _strstr - finds first occurence of a substring
+ * @haystack: string to be looked through
+ * @needle: string to be found 
+ * Returm: pointer to the first occurence
+ */
+char *_strstr(char *haystack, char *needle)
+{
+}
